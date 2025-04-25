ef82a50e9d9f81ef82ec18588bb9cf3ff8146dd4
Josh Bonhage <joshmbonhage@gmail.com>
2025-04-25 17:01:09 -0500

    designed hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
 import Head from "next/head";
+import { FormInput } from "lucide-react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,21 +18,27 @@ export default function Home() {
       <link rel="icon" href="/favicon.ico" />
   </Head>
 
-      <Navbar />
-    <div className='flex min-h-screen w-full items-center justify-center'>  
-      <main>
-        <section>
-        <h1 className="text-2xl text-center font-bold">
+    <Navbar />
+
+    <div>
+      <main className="flex min-h-screen p-24 pr-80 pl-80 justify-center">
+        <section className='flex flex-col flex-grow w-full mt-50 justify-center items-center'>
+        <div>
+        <h1 className="text-lg text-center">
           Coming Soon
         </h1>
-        <h1 className='text-5xl font-bold'>
-        Real-Time Amazon Data. No Bloat.
+        <h1 className='text-5xl font-bold text-center'>
+          <span className='text-teal-400'>Real-Time </span> Amazon Data. <br/> No Bloat. 
         </h1>
-        <div className="flex flex-row-reverse justify-center  items-center gap-4">
-        <button className="bg-blue-500 text-white rounded-md px-4 py-2 mt-4 ml-2">
-            Sign up for our waitlist.
-          </button>
-          <input type="text" placeholder="Enter your email" className="border border-gray-300 rounded-md p-2 mt-4 flex flex-auto items-center" />
+        <div className="flex flex-row-reverse gap-4 items-center mt-8">
+        <button className="bg-blue-500 text-white rounded-md px-4 py-2 ml-2">
+          Join our waitlist!
+        </button>
+          <input type="text" placeholder="Enter your email" className="flex flex-grow border border-gray-300 rounded-md p-2 " />      
+        </div>
+        </div>
+        <div className=" flex flex-grow w-full mt-20 border border-white rounded-md pt-80 pb-80 ">
+
         </div> 
           </section>
       </main>
